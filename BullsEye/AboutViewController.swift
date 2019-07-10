2ef5f5041131a6f80cdd2e1e4ed6c2a35df4058d
serendiipity <imane.chk@gmail.com>
2019-07-11 00:03:03 +0100

    create AboutViewController and segue

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by imane on 7/10/19.
+//  Copyright © 2019 imane. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
